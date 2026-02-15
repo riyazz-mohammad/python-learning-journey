3e53332cd13ef57df16a29eff214a9974a66460e
riyaz <riyazuddinnmohammad@gmail.com>
2026-02-15 13:26:47 +0530

    Add data_types

---
+++ data_types.py
@@ -0,0 +1,12 @@
+
+my_string = "Hello"
+my_integer = 42
+my_float = 3.14
+my_boolean = True
+my_list = [1, 2, 3, 4, 5]
+
+print("String:", my_string)
+print("Integer:", my_integer)
+print("Float:", my_float)
+print("Boolean:", my_boolean)
+print("List:", my_list)
