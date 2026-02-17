6808fb986e0cd7fb9b62c8016b7ef29912478d81
riyaz <riyazuddinnmohammad@gmail.com>
2026-02-17 18:45:10 +0530

    Add age checker with conditional

---
+++ age_checker.py
@@ -0,0 +1,13 @@
+age=int(input("Enter your age:"))
+if age>=18:
+    print("You can Vote!")
+else:
+    print("Too Young to Vote!")
+if age>=16:
+    print("You can Drive!")
+else:
+    print("Too Young to Drive!")
+if age>=21:
+    print("You can Legally Drink")
+else:
+    print("Not yet Drinking age!")
